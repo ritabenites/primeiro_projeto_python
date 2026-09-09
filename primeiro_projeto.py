786c8db51ec47bbaa476253779c65d1cb81a0af6
Rita de Cássia Barbosa Benites <barbosabenitesritadecassia@gmail.com>
2026-09-09 09:10:41 -0300

    Add files via upload

---
+++ primeiro_projeto.py
@@ -0,0 +1,10 @@
+nome = "Rita de Cássia Barbosa Benites"
+curso = "Ciência da Computação"
+instituição = "IFMS"
+objetivo = "Estágio na área de tecnologia"
+
+
+print("nome:", nome)
+print("curso:",curso)
+print("instituição:",instituição)
+print("objetivo:",objetivo)
